8fac55d2e649b96dae70c795bcf84f0e95927610
Leonid Knyazev <leonid@knyazev.me>
2018-02-02 14:33:22 +0300

    Logger system + Revert Bot loader from testing Loggers...
    update #1

--- app/index.js
+++ app/index.js
@@ -1,24 +1,11 @@
 // $ID: index.js.js, 10 Jan 2018, 17:55, Leonid 'n3o' Knyazev $
 
-import { Logger } from './utils';
+import bot from './bot';
 
-const logger = new Logger();
-
-// console.log('====================================');
-// console.log(logger);
-console.log('====================================');
-logger.info('Testing!', {section: true});
-logger.info('Test Info Message!');
-logger.debug('Test Debug Message!');
-logger.error('Test Error Message!');
-console.log('====================================');
-
-// import bot from './bot';
-//
-// (async () => {
-//     try {
-//         await bot.start();
-//     } catch (error) {
-//         bot.log.error(error.message);
-//     }
-// })();
+(async () => {
+    try {
+        await bot.start();
+    } catch (error) {
+        bot.log.error(error.message);
+    }
+})();
